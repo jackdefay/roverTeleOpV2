438a1beab827d6f5fb698b4cba8a8a88c325629b
3jackattack3 <3jackattack3@gmail.com>
2018-03-04 17:47:34 -0500

    got rid of extra stuff

--- onBoard/src/main.cpp
+++ onBoard/src/main.cpp
@@ -1,9 +1,93 @@
 #include <Arduino.h>
 
+#define JOYSTICK_RANGE 1023
+
+//motor direction pins
+#define rfpos A5
+#define rfneg A2
+#define rbpos A1
+#define rbneg 5
+#define lfpos 11
+#define lfneg 10
+#define lbpos 9
+#define lbneg 6
+
+//motor speed pins
+#define pwmrf 13
+#define pwmrb 12
+#define pwmlf A3
+#define pwmlb A4
+
+void setDirection(char motor, bool direction);
+void setSpeed(int pwmr, int pwml);
+int clip(int num);
+
 void setup() {
-    // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  Serial.println("starting");
+
+  //motor pins
+  pinMode(rfpos, OUTPUT);
+  pinMode(rfneg, OUTPUT);
+  pinMode(rbpos, OUTPUT);
+  pinMode(rbneg, OUTPUT);
+  pinMode(lfpos, OUTPUT);
+  pinMode(lfneg, OUTPUT);
+  pinMode(lbpos, OUTPUT);
+  pinMode(lbneg, OUTPUT);
+  pinMode(pwmrf, OUTPUT);
+  pinMode(pwmrb, OUTPUT);
+  pinMode(pwmlf, OUTPUT);
+  pinMode(pwmlb, OUTPUT);
+
+  //set motors to forwards
+  setDirection('r', true);
+  setDirection('l', true);
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void loop(){
+  setSpeed(255, 255);
+}
+
+void setDirection(char motor, bool direction){  //1 = forwards, 0 = backwards
+  Serial.print("setting direction to "); Serial.println(direction);
+  if(motor == 'r'){
+    digitalWrite(rfpos, (int) direction);
+    digitalWrite(rfneg, (int) !direction);
+    digitalWrite(rbpos, (int) direction);
+    digitalWrite(rbneg, (int) !direction);
+  }
+  else if(motor == 'l'){                        //the left wheels are reversed
+    digitalWrite(lfpos, (int) !direction);
+    digitalWrite(lfneg, (int) direction);
+    digitalWrite(lbpos, (int) !direction);
+    digitalWrite(lbneg, (int) direction);
+  }
+}
+
+void setSpeed(int pwmr, int pwml){
+  if(pwmr>=0) setDirection('r', true);
+  else setDirection('r', false);
+
+  if(pwml>=0) setDirection('l', true);
+  else setDirection('l', false);
+
+  pwmr = clip(pwmr);
+  pwml = clip(pwml);
+
+  Serial.print("the values being written to the motors are: "); Serial.print(pwmr); Serial.print(", "); Serial.println(pwml);
+
+  analogWrite(pwmrf, pwmr);
+  analogWrite(pwmrb, pwmr);
+  analogWrite(pwmlf, pwml);
+  analogWrite(pwmlb, pwml);
+}
+
+int clip(int num){
+  if(num>=-100 && num<=100) return 0;
+  if(num>=0 && num<=255) return num;
+  else if(num>=-255 && num<0) return -num;
+  else if(num>255 || num<-255) return 255;
+  else return 0;
 }
